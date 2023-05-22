b058d7d2790370c2412885d339b791fcf2b33edf
elanurimirgi <116424565+elanurimirgi@users.noreply.github.com>
2023-05-22 12:52:40 +0300

    Add files via upload

---
+++ ödev4/index.js
@@ -0,0 +1,15 @@
+const fs = require ('fs');
+/*fs.writeFile('create.json', ' {"name": "Employee 1 Name", "salary": 2000}', 'utf8',  (err) => {
+    if (err) console.log(err);
+});*/
+
+/*fs.readFile('create.json','utf8', (err) => {
+    if (err) console.log(err);
+    
+});*/
+/*fs.appendFile('create.json', '\n nodejs dosya işlemleri', 'utf8', (err) => {
+    if (err) console.log(err);
+});*/
+/*fs.unlink('create.json', (err) => {
+    if (err) console.log(err);
+});*/
